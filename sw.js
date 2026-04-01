a56fbf8b5ab2224f357085ad44d6fd0eb4c6c812
alikural0 <alikural0@gmail.com>
2026-04-01 18:19:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
+const CACHE_NAME = "artist-v1";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("tiyatro-v1").then(cache => {
-      return cache.addAll(["index.html"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/Artist/",
+        "/Artist/index.html",
+        "/Artist/manifest.json",
+        "/Artist/icon-192.png",
+        "/Artist/icon-512.png",
+        "/Artist/btn-oyun.png",
+        "/Artist/btn-hesap.png",
+        "/Artist/btn-rapor.png"
+      ]);
     })
   );
 });
